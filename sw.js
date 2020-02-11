b6e52c195b3e39966bf6c08dea6d50868857e375
AlexeyShaykov <kayfolom@inbox.ru>
2020-02-11 15:49:47 +0300

    wip

--- sw.js
+++ sw.js
@@ -1,21 +1,7 @@
 self.addEventListener('install', function(e) {
 	e.waitUntil(
 	  caches.open('video-store').then(function(cache) {
-		return cache.addAll([
-		  '/PWATest',
-		  '/PWATest/index.html',
-		  '/PWATest/script.js',
-		  '/PWATest/style.css',
-		]);
+		return cache.addAll([]);
 	  })
 	);
-   });
-   
-   self.addEventListener('fetch', function(e) {
-	 console.log(e.request.url);
-	 e.respondWith(
-	   caches.match(e.request).then(function(response) {
-		 return response || fetch(e.request);
-	   })
-	 );
-   });
+});
